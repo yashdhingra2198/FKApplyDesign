bacd159ae00d25bd99911bbc0d6bf7eb2c3e0105
Yash Dhingra <yashdhingra2198@gmail.com>
2020-01-25 12:11:56 +0530

    commit number-2 	commit name - checkwin function implemented
    	description: while there is no winner ,users gives input by turn and win is checked by checkwin function
    	todo : improvement and optimisations(if any) in code

--- ticTacToe.java
+++ ticTacToe.java
@@ -8,7 +8,9 @@ public class ticTacToe{
         public static final int COLS=3;
         public static String[][] board=new String[ROWS][COLS];
         public static Scanner in = new Scanner(System.in);
-        Static String winner=null;
+        static String winner=null;
+        static String turn;
+        static int count=0;
         
 
 
@@ -16,20 +18,67 @@ public class ticTacToe{
         {
 
             initialiseBoard();
+            int x,y;
+            turn="X";
+            while(winner==null)
+            {
+                count++;
+                if(count==10)
+                {
+                    printBoard();
+                    System.out.println("The match is a draw");
+
+                    break;
+                }
+                printBoard();
+                System.out.println("User "+ turn+",please enter the coordinates(starting from 1) where you want to mark");
+                 x=in.nextInt();
+                 y=in.nextInt();
+                 if(turn=="X"){
+                     board[x-1][y-1]="X";
+                 }
+                 else
+                 {
+                     board[x-1][y-1]="O";
+                 }
+
+                 checkwin(x-1,y-1);
+
+                 if(winner=="X"){
+                     System.out.println("X won the match");
+                     break;
+                 }
+                 else if(winner=="O") {
+                     System.out.println("O won the match");
+                     break;
+                 }
+                if(turn=="X")
+                turn="O";
+                else
+                turn="X";
 
 
-
-
-            printBoard();
-
+            }
 
         }
+
+
+        public static void checkwin(int row,int col){
+
+            if((turn.equals(board[row][0]) && turn.equals(board[row][1]) && turn.equals(board[row][2]))
+                    ||(turn.equals(board[0][col]) && turn.equals(board[1][col]) && turn.equals(board[2][col]))
+                    ||(row==col && turn.equals(board[0][0]) && turn.equals(board[1][1]) && turn.equals(board[2][2]))
+                    ||((row+col)==2 && turn.equals(board[0][2]) && turn.equals(board[1][1]) && turn.equals(board[2][0])))
+                winner=turn;
+
+        }
+
 
         public static void initialiseBoard(){
 
             for(int row=0;row<board.length;row++)
                 for(int col=0;col<board[row].length;col++){
-                    board[row][col]="";
+                    board[row][col]=" ";
                 }
 
         }
