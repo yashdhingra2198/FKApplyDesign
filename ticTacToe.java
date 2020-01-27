7526b51351c9274978823c78068726e2c80e4d02
Yash Dhingra <yashdhingra2198@gmail.com>
2020-01-28 00:10:28 +0530

    commit number -1 	commit name - added machine vs human code with interface and manager class 	commit descriotion : created interface with human and computer class implementing that interface and a manager class for game
    	modified:   classFiles/ticTacToe.class
    	modified:   ticTacToe.java

--- ticTacToe.java
+++ ticTacToe.java
@@ -1,87 +1,117 @@
 
 import java.util.*;
 
-interface Drawable{
-    void printBoard();
+interface gameplay{
+    void play();
 }
 
+class human implements gameplay{
 
-public class ticTacToe implements Drawable{
+    Scanner in = new Scanner(System.in);
+    public void play() {
+        int x, y;
 
-        public static final int ROWS=3;
-        public static final int COLS=3;
-        public static String[][] board=new String[ROWS][COLS];
-        public static Scanner in = new Scanner(System.in);
-        static String winner=null;
-        static String turn;
-        static int count=0;
-        
+        manager managerObj = new manager();
+
+        System.out.println("User " + managerObj.turn + ",please enter the (x,y) coordinates(starting from 1) where you want to mark");
+        x = in.nextInt();
+        y = in.nextInt();
+        managerObj.x=x;
+        managerObj.y=y;
+        if (managerObj.turn == "X") {
+            managerObj.board[x - 1][y - 1] = "X";
+        } else {
+            managerObj.board[x - 1][y - 1] = "O";
+        }
+
+    }
+
+}
+
+class computer implements gameplay{
 
 
-        public static void main(String args[])
-        {
-
-            initialiseBoard();
-            int x,y;
-            turn="X";
-            Drawable d=new ticTacToe();
-            while(winner==null)
-            {
-                count++;
-                if(count==ROWS*COLS+1)
-                {
-                    d.printBoard();
-                    System.out.println("The match is a draw");
-
+    public void play() {
+        int flag=0;
+        manager managerObj = new manager();
+        for (int i = 0; i < ticTacToe.sizeofmatrix; i++) {
+            for (int j = 0; j < ticTacToe.sizeofmatrix; j++) {
+                if ((managerObj.board[i][j]).equals(" ")) {
+                    managerObj.board[i][j] = "X";
+                    managerObj.x=i+1;
+                    managerObj.y=j+1;
+                    flag=1;
                     break;
+
                 }
-
-                d.printBoard();
-
-                System.out.println("User "+ turn+",please enter the coordinates(starting from 1) where you want to mark");
-                 x=in.nextInt();
-                 y=in.nextInt();
-                 if(turn=="X"){
-                     board[x-1][y-1]="X";
-                 }
-                 else
-                 {
-                     board[x-1][y-1]="O";
-                 }
-
-                 checkwin(x-1,y-1);
-
-                 if(winner=="X"){
-                     System.out.println("X won the match");
-                     break;
-                 }
-                 else if(winner=="O") {
-                     System.out.println("O won the match");
-                     break;
-                 }
-                if(turn=="X")
-                turn="O";
-                else
-                turn="X";
 
 
             }
-
+            if(flag==1)
+                break;
         }
+    }
 
 
-        public static void checkwin(int row,int col){
+}
 
-            if((turn.equals(board[row][0]) && turn.equals(board[row][1]) && turn.equals(board[row][2]))
+class manager{
+
+    static String[][] board=new String[ticTacToe.sizeofmatrix][ticTacToe.sizeofmatrix ];
+
+        String winner=null;
+    static String turn;
+    static int x,y;
+
+
+         void checkwin(int row,int col){
+
+             boolean val=true;
+             for(int j=0;j<ticTacToe.sizeofmatrix;j++)
+             {
+                 val = val && turn.equals(board[row][j]);
+             }
+             if(val==false)
+             {
+                 int i;
+                 val=true;
+                 for( i=0;i<ticTacToe.sizeofmatrix;i++) {
+                     val = val && turn.equals(board[i][col]);
+                 }
+                 if(val==false&&row==col)
+                 {
+                     val=true;
+                     for( i=0;i<ticTacToe.sizeofmatrix;i++) {
+                         val = val && turn.equals(board[i][i]);
+                     }
+                 }
+                 else if(val==false&&row+col==2)
+                 {
+                     val=true;
+                     int jj=ticTacToe.sizeofmatrix-1;
+                     for(i=0;i<ticTacToe.sizeofmatrix;i++) {
+
+                         val = val && turn.equals(board[i][jj]);
+                         jj--;
+                     }
+                 }
+             }
+             if(val==true)
+                 winner=turn;
+
+
+
+
+            /*if((turn.equals(board[row][0]) && turn.equals(board[row][1]) && turn.equals(board[row][2]))
                     ||(turn.equals(board[0][col]) && turn.equals(board[1][col]) && turn.equals(board[2][col]))
                     ||(row==col && turn.equals(board[0][0]) && turn.equals(board[1][1]) && turn.equals(board[2][2]))
                     ||((row+col)==2 && turn.equals(board[0][2]) && turn.equals(board[1][1]) && turn.equals(board[2][0])))
-                winner=turn;
+                winner=turn;*/
 
         }
 
 
-        public static void initialiseBoard(){
+         void initialiseBoard(){
 
             for(int row=0;row<board.length;row++)
                 for(int col=0;col<board[row].length;col++){
@@ -90,7 +120,7 @@ public class ticTacToe implements Drawable{
 
         }
 
-        public void printBoard(){
+         void printBoard(){
             for(int row=0;row<board.length;row++) {
                 for (int col=0;col<board[row].length;col++) {
                     System.out.print(board[row][col] + " , ");
@@ -99,10 +129,71 @@ public class ticTacToe implements Drawable{
             }
 
         }
+}
 
+class ticTacToe {
 
+    static int sizeofmatrix;
 
+        public static void main(String args[])
+        {
 
+            Scanner in = new Scanner(System.in);
 
+            System.out.println("Enter the size of matrix");
+            sizeofmatrix=in.nextInt();
+
+            int numberOfTurns=0;
+            //manager obj=new manager();
+            int key;
+            while(true) {
+                while(true) {
+                    System.out.println("Press 1 for human vs human game or 2 to play with computer and 3 for exit");
+                    key = in.nextInt();
+                    if (key == 3)
+                        System.exit(0);
+                    if(key==1||key==2)
+                        break;
+                }
+                manager obj=new manager();
+                obj.initialiseBoard();
+                while (Objects.isNull(obj.winner)) {
+                    numberOfTurns++;
+                    if (numberOfTurns == sizeofmatrix * sizeofmatrix + 1) {
+                        obj.printBoard();
+                        System.out.println("The match is a draw");
+
+                        break;
+                    }
+                    obj.printBoard();
+                    if (numberOfTurns == 1)
+                        obj.turn = "X";
+                    if (key == 1 || numberOfTurns % 2 == 0) {
+
+                        human h = new human();
+                        h.play();
+                    }
+                    if (key == 2 && numberOfTurns % 2 == 1) {
+                        computer m = new computer();
+                        m.play();
+                    }
+
+                    obj.checkwin((obj.x) - 1, (obj.y) - 1);
+
+                    if (obj.winner == "X") {
+                        System.out.println("X won the match");
+                        break;
+                    } else if (obj.winner == "O") {
+                        System.out.println("O won the match");
+                        break;
+                    }
+                    if (obj.turn == "X")
+                        obj.turn = "O";
+                    else
+                        obj.turn = "X";
+                }
+            }
+
+        }
 
 }
